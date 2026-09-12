40dd45dad5943bd395b0831a42b6f7afcb78005e
LTCjRet <31146487+LTCjRet@users.noreply.github.com>
2026-09-11 21:52:42 -0400

    Add files via upload

---
+++ assets/ground-data.js
@@ -0,0 +1,220 @@
+/* Generated. Bounds, plat placement and grave positions for 14-the-ground.html.
+   Regenerate with make_overlays.py; the images must match these bounds exactly. */
+window.KA_DATA = {
+ "bounds": [
+  [
+   49.26396218703662,
+   4.923317437632083
+  ],
+  [
+   49.301251276307845,
+   5.007107075806954
+  ]
+ ],
+ "plat": {
+  "image": "assets/plat-a122-geo.webp",
+  "bounds": [
+   [
+    49.28001208701316,
+    4.972286806305139
+   ],
+   [
+    49.28824722074202,
+    4.985593063248229
+   ]
+  ]
+ },
+ "graves": {
+  "type": "FeatureCollection",
+  "features": [
+   {
+    "type": "Feature",
+    "geometry": {
+     "type": "Point",
+     "coordinates": [
+      4.981748,
+      49.283011
+     ]
+    },
+    "properties": {
+     "grave": "8",
+     "name": "Sgt Alexander Dudley Bryant",
+     "note": "Co. L, 110th Infantry. Listed on the plat as \"No tag\".",
+     "lambert93": [
+      844203,
+      6911074
+     ],
+     "uncertainty_m": 30,
+     "source": "GRS Plat A-122, Det 1st Bn 29th Engineers, rechecked 2-4 Jan 1919 (NARA)"
+    }
+   },
+   {
+    "type": "Feature",
+    "geometry": {
+     "type": "Point",
+     "coordinates": [
+      4.981806,
+      49.282977
+     ]
+    },
+    "properties": {
+     "grave": "1",
+     "name": "Charles W. White",
+     "note": "1905098. Survey Initial Point for the sheet.",
+     "lambert93": [
+      844207,
+      6911071
+     ],
+     "uncertainty_m": 30,
+     "source": "GRS Plat A-122, Det 1st Bn 29th Engineers, rechecked 2-4 Jan 1919 (NARA)"
+    }
+   },
+   {
+    "type": "Feature",
+    "geometry": {
+     "type": "Point",
+     "coordinates": [
+      4.980893,
+      49.283093
+     ]
+    },
+    "properties": {
+     "grave": "9",
+     "name": "Unidentified on sheet",
+     "note": "Plotted beside Sgt James B. Patton, Co. K, 110th Inf.",
+     "lambert93": [
+      844140,
+      6911082
+     ],
+     "uncertainty_m": 30,
+     "source": "GRS Plat A-122, Det 1st Bn 29th Engineers, rechecked 2-4 Jan 1919 (NARA)"
+    }
+   },
+   {
+    "type": "Feature",
+    "geometry": {
+     "type": "Point",
+     "coordinates": [
+      4.979565,
+      49.283511
+     ]
+    },
+    "properties": {
+     "grave": "10",
+     "name": "B. Grigaliunas",
+     "note": "1896821",
+     "lambert93": [
+      844043,
+      6911126
+     ],
+     "uncertainty_m": 30,
+     "source": "GRS Plat A-122, Det 1st Bn 29th Engineers, rechecked 2-4 Jan 1919 (NARA)"
+    }
+   },
+   {
+    "type": "Feature",
+    "geometry": {
+     "type": "Point",
+     "coordinates": [
+      4.979523,
+      49.283537
+     ]
+    },
+    "properties": {
+     "grave": "11",
+     "name": "John Kobes",
+     "note": "1909712",
+     "lambert93": [
+      844040,
+      6911129
+     ],
+     "uncertainty_m": 30,
+     "source": "GRS Plat A-122, Det 1st Bn 29th Engineers, rechecked 2-4 Jan 1919 (NARA)"
+    }
+   },
+   {
+    "type": "Feature",
+    "geometry": {
+     "type": "Point",
+     "coordinates": [
+      4.979823,
+      49.28472
+     ]
+    },
+    "properties": {
+     "grave": "12",
+     "name": "Stefano Graziano",
+     "note": "1908648. Plotted on the hedge line, not the road.",
+     "lambert93": [
+      844058,
+      6911261
+     ],
+     "uncertainty_m": 30,
+     "source": "GRS Plat A-122, Det 1st Bn 29th Engineers, rechecked 2-4 Jan 1919 (NARA)"
+    }
+   },
+   {
+    "type": "Feature",
+    "geometry": {
+     "type": "Point",
+     "coordinates": [
+      4.976032,
+      49.284532
+     ]
+    },
+    "properties": {
+     "grave": "13",
+     "name": "T. Holleran",
+     "note": "2427154",
+     "lambert93": [
+      843783,
+      6911233
+     ],
+     "uncertainty_m": 30,
+     "source": "GRS Plat A-122, Det 1st Bn 29th Engineers, rechecked 2-4 Jan 1919 (NARA)"
+    }
+   },
+   {
+    "type": "Feature",
+    "geometry": {
+     "type": "Point",
+     "coordinates": [
+      4.9762,
+      49.284518
+     ]
+    },
+    "properties": {
+     "grave": "14",
+     "name": "Alex Moro",
+     "note": "1909644",
+     "lambert93": [
+      843795,
+      6911232
+     ],
+     "uncertainty_m": 30,
+     "source": "GRS Plat A-122, Det 1st Bn 29th Engineers, rechecked 2-4 Jan 1919 (NARA)"
+    }
+   },
+   {
+    "type": "Feature",
+    "geometry": {
+     "type": "Point",
+     "coordinates": [
+      4.980351,
+      49.283454
+     ]
+    },
+    "properties": {
+     "grave": "anchor",
+     "name": "Road fork",
+     "note": "Georeferencing anchor: the Y-junction SW of Ferme des Granges.",
+     "lambert93": [
+      844100,
+      6911121
+     ],
+     "uncertainty_m": 5
+    }
+   }
+  ]
+ }
+};
